3a1b7596a8218ad071fd5a93cfce0cb5c74d6e61
Nurlan328 <nurlan.nurlanich@gmail.com>
2022-10-08 21:06:38 +0600

    Sample of restful web services with to do list project using Spring Security

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'Elon Musk', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'Jeff Bezos', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'Mark Zuckerberg', 'Learn Microservices', sysdate(), false);
